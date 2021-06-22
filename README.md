41d909779fa7dc023f91a9456a0aedd7a8788195
Gordon Hamilton <gordon@Gordons-MacBook-Air.local>
2021-06-22 13:54:36 -0400

    maestro is setup with mongodb zooniverse database and events can be stored in database

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Start the backend server using the command:
 ### Accessing Route Endpoints
 To access or test any route endpoints, you need the name of the backend which listed when maestro starts up or is found im backend.yml. The backend name along with swagger needs to be apart of base url to view the toures endpoints which are controlled by Swagger.
 
-Syntax
+Syntax:
 >27.0.0.1:8000/zooniverse-db/swagger/
 
 ### Adding dependencies 
--- zoondb/backend/rest.py
+++ zoondb/backend/rest.py
@@ -25,81 +25,42 @@ from zoondb.routines import composite, simple
 # NOTE: The URL Prefix for your backend has to be the name of the backend
 blueprint = Blueprint("zoondb Backend", url_prefix="/")
 
-# # Create a new connection to a single MongoDB instance at host:port.
-# client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:8000")
-
-# # # Connection to a database 
-# db = client['zooniverseDB']
-
-
-@doc.summary("Hello from zooniverse-db!")
-@blueprint.get("hello")
-async def hello(request: Request) -> HTTPResponse:
-    """Hello World.
-
-    Parameters
-    ----------
-    request : Request
-        Request object from sanic app
-
-    Returns
-    -------
-    HTTPResponse
-    """
-    return json("Hello from zooniverse-db 🦧")
-
-
 # @doc.consumes(schema.Event)
 @blueprint.post("transfer-event")
 @doc.summary("CHIME/FRB event data is being sent to server so that it can be stored in zooniverse database")
 async def get_event_data_from_CHIME(request):
     try:
+        client = request.app.mongo.client
         data = request.args
-        event_number = data["event_number"]
+
+        # Should this event number be used as the event id?
+        event_number = data["event"]
         dm_value = data["dm"]
         snr_value = data["snr"]
+        beams_value = data["beams"]
         data_path = data["data_path"]
+        transfer_status = data["transfer_status"]
+
+        # code to add new event to database 
+        document = {
+            'event': event_number,
+            'dm': dm_value, 
+            'snr': snr_value,
+            'beams': beams_value, 
+            'data_paths': data_path,
+            'transfer_status': transfer_status,
+            # 'zooniverse_classification': 'INCOMPLETE',
+            # 'expert_classification': 'INCOMPLETE'
+        }
+        result = await client.zooniverseDB.events.insert_one(document)
+        print('result %s' % repr(result.inserted_id))
         return  json(True)
-    #     # code to add to database here
 
     except Exception as error:
         print(str(error))
         return json(str(error))
 
-# # Stores new events in database
-# @app.route("", methods=['POST'])
-# def addEventToDB(request):
-#     if request.method == "POST":
-#         event_path= requests.get("https://frb.chimenet.ca/frb-master/v1/events/datapaths/147503727", headers=auth)
-# async def test(request):
-#     return json({ "hello": "world" })
-
-# display event data path
-# @app.route("/", methods=['POST'])
-# def addEventToDB():
-#     if request.method == "POST":
-#         event_path= requests.get("https://frb.chimenet.ca/frb-master/v1/events/datapaths/147503727", headers=auth)
-# async def test(request):
-    # return json({ "hello": "world" })
-# why in this route am I adding to the database? should this be named, just add events to database?
-# @doc.summary("Add event to the transfer list to send to zooniverse.")
-# @doc.consumes(schema.Event)
-# @blueprint.post("/event")
-# async def add_event(request):
-#     try:
-
-#         client = request.app.mongo.client
-#         await client.zooniverse.events.insert_one(request.json)
-
-#         # await db.events.insert_one(request.json)
-
-#         return json(True)
-#     except Exception as e:
-#         print(str(e))
-#         raise
-
-
-@doc.summary("Fetch event")
+@doc.summary("Fetch an event")
 # @doc.consumes(schema.Event)
 @blueprint.get("event/<event_no>")
 async def get_event(request, event_no):
@@ -118,23 +79,31 @@ async def get_event(request, event_no):
         print(str(e))
         raise
 
-
 @doc.summary("Fetch all events")
 # @doc.consumes(schema.Event)
-@blueprint.get("events")
-async def fetch_all_events(request):
+@blueprint.get("all-events")
+async def getAllEvents(request):
     try:
         client = request.app.mongo.client
-        items = []
-        docs = client.zooniverse.events.find({}, projection={"_id": 0})
-        async for d in docs:
-            items.append(d)
-        return json(items)
-    except Exception as e:
-        print(str(e))
-        raise
+        total_number_of_events = await client.zooniverseDB.events.count_documents({})
+        print("total number of events: ", total_number_of_events)
+        
+        # cursor = client.zooniverseDB.events.find({})
+        # for document in await cursor.to_list(length=100):
+        #     print(document)
 
+        # items = []
+        # docs = client.zooniverse.events.find({}, projection={"_id": 0})
+        # async for d in docs:
+        #     items.append(d)
+        # return json(items)
 
+        return  json(True)
+ 
+    except Exception as error:
+        print(str(error))
+        return json(str(error))
+        
 @doc.summary("Update event.")
 # @doc.consumes(schema.Event)
 @blueprint.put("event/<event_no>")
@@ -150,7 +119,6 @@ async def update_event(request, event_no):
         print(str(e))
         raise
 
-
 @doc.summary("Delete event")
 # @doc.consumes(schema.Event)
 @blueprint.delete("event/<event_no>")
@@ -162,7 +130,6 @@ async def delete_event(request, event_no):
     except Exception as e:
         print(str(e))
         raise
-
 
 @doc.summary("Fetch events to transfer to zooniverse.")
 # @doc.produces([schema.Event])
@@ -182,7 +149,6 @@ async def fetch_events_for_transfer(request):
         print(str(e))
         raise
 
-
 @doc.summary("Fetch events to transfer to cleanup.")
 # @doc.produces([schema.Event])
 @blueprint.get("events-for-cleanup")
@@ -200,7 +166,6 @@ async def fetch_events_for_cleanup(request):
     except Exception as e:
         print(str(e))
         raise
-
 
 @doc.summary("Fetch events for expert verification.")
 # @doc.produces([schema.Event])
@@ -223,7 +188,6 @@ async def fetch_events_for_experts(request):
         print(str(e))
         raise
 
-
 @doc.summary("Add zooniverse classification")
 @blueprint.put("zooniverse-classification/<event_no>/<classification>")
 async def zooniverse_classification(request, event_no, classification):
@@ -240,7 +204,6 @@ async def zooniverse_classification(request, event_no, classification):
         print(str(e))
         raise
 
-
 @doc.summary("Add expert classification")
 @blueprint.put("expert-classification/<event_no>/<classification>")
 async def expert_classification(request, event_no, classification):
@@ -256,12 +219,6 @@ async def expert_classification(request, event_no, classification):
     except Exception as e:
         print(str(e))
         raise
-
-@doc.summary("Upload zooniverse classification report")
-# @doc.produces([schema.ZooniverseClassificationReport])
-@blueprint.post("classifications")
-async def upload_classification_report(request):
-    return json("Hello from zooniverse 🦧")
 
 # =========
 
@@ -299,3 +256,20 @@ async def upload_classification_report(request):
 #     """
 #     example = composite.Composite(1.0, 10.0, "hex")
 #     return json(example.get_random_integer())
+
+# @doc.summary("Hello from zooniverse-db!")
+# @blueprint.get("hello")
+# async def hello(request: Request) -> HTTPResponse:
+#     """Hello World.
+
+#     Parameters
+#     ----------
+#     request : Request
+#         Request object from sanic app
+
+#     Returns
+#     -------
+#     HTTPResponse
+#     """
+
+#     return json("Hello from zooniverse-db 🦧")
